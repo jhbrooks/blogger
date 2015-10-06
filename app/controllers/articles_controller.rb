67446d52ad7409e44478fabb0cbec7e84396c082
James Brooks <jhbrooks@uchicago.edu>
2015-10-06 15:36:06 -0400

    added navigation links to isolated views, added deletion of empty tags during article destroy and update actions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,8 +25,20 @@ class ArticlesController < ApplicationController
 
 	def destroy
 		@article = Article.find(params[:id])
+		deleted_tag_names = []
+		@article.tags.each do |tag|
+			if !(Tagging.where(tag_id: tag.id).where.not(article_id: @article.id).exists?)
+				deleted_tag_names << tag.name
+				tag.destroy
+			end
+		end
 		@article.destroy
-		flash.notice = "Article '#{@article.title}' deleted!"
+
+		if deleted_tag_names == []
+			flash.notice = "Article '#{@article.title}' deleted!"
+		else
+			flash.notice = "Article '#{@article.title}' deleted! Also deleted the following empty tags: #{deleted_tag_names.join(", ")}"
+		end
 
 		redirect_to articles_path
 	end
@@ -37,8 +49,27 @@ class ArticlesController < ApplicationController
 
 	def update
 		@article = Article.find(params[:id])
-		flash.notice = "Article '#{@article.title}' updated!"
+		old_tag_names = @article.tag_list.split(", ")
+		old_article_title = @article.title
+
 		@article.update(article_params)
+		new_tag_names = @article.tag_list.split(", ")
+
+		threatened_tag_names = old_tag_names - new_tag_names
+		threatened_tags = Tag.where(name: threatened_tag_names)
+		deleted_tag_names = []
+		threatened_tags.each do |tag|
+			if !(Tagging.where(tag_id: tag.id).where.not(article_id: @article.id).exists?)
+				deleted_tag_names << tag.name
+				tag.destroy
+			end
+		end
+
+		if deleted_tag_names == []
+			flash.notice = "Article '#{old_article_title}' updated!"
+		else
+			flash.notice = "Article '#{old_article_title}' updated! Also deleted the following empty tags: #{deleted_tag_names.join(", ")}"
+		end
 
 		redirect_to article_path(@article)
 	end
